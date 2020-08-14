0a8fa4c12e57e1c338e0586f1e61c3c7a2f313a2
Yasin Razlik <yrazlik@gmail.com>
2020-08-14 22:41:43 +0300

    fix

--- src/main/java/com/yrazlik/lol/controller/SummonerController.java
+++ src/main/java/com/yrazlik/lol/controller/SummonerController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.JsonSyntaxException;
+import com.yrazlik.lol.request.RequestGetActiveGame;
 import com.yrazlik.lol.request.RequestGetSummonerByName;
 import com.yrazlik.lol.request.RequestSearchSummonerByName;
 import com.yrazlik.lol.response.BaseResponse;
+import com.yrazlik.lol.response.GetActiveGameInfoResponse;
 import com.yrazlik.lol.response.GetSummonerByNameResponse;
 import com.yrazlik.lol.response.SearchSummonerByNameResponse;
 import com.yrazlik.lol.service.SummonerService;
@@ -40,6 +42,24 @@ public class SummonerController {
 		RequestSearchSummonerByName requestModel = new RequestSearchSummonerByName(language, region, summonerName);
 		SearchSummonerByNameResponse response = summonerService.searchSummonerByName(requestModel);
 		return new BaseResponse<SearchSummonerByNameResponse>(response);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/active-games/by-summoner/{region}/{summonerName}.json")
+	public BaseResponse<GetActiveGameInfoResponse> getActiveGameInfo(HttpServletRequest request, @PathVariable String summonerName, @PathVariable String region) throws JsonSyntaxException, IOException {
+		String language = request.getHeader(PlatformConstants.HEADER_LANGUAGE);
+		if(region == null || region.equals("")) {
+			region = request.getHeader(PlatformConstants.HEADER_REGION);
+		}
+		RequestGetActiveGame requestModel = new RequestGetActiveGame();
+		requestModel.setLanguage(language);
+		requestModel.setRegion(region);
+		requestModel.setSummonerName(summonerName);
+		GetActiveGameInfoResponse response = summonerService.getActiveGameInfo(requestModel);
+		BaseResponse<GetActiveGameInfoResponse> result = new BaseResponse<GetActiveGameInfoResponse>(response);
+		if(response.getGameId() == 0) {
+			result.setStatus("NOK");
+		}
+		return result;
 	}
 	
 }
